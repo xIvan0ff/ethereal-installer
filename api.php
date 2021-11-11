3a892fdd33937f8ae97837196ea3ffb1411bb6f3
xIvan0ff <hrisivanov2@gmail.com>
2021-11-11 18:31:16 +0200

    revert to shell_exec

--- api.php
+++ api.php
@@ -81,5 +81,5 @@ if ($method == "stop") {
     $command = "pkill $host -f";
 }
 
-$output = system($command . " 2>&1");
+$output = shell_exec($command . " 2>&1");
 die(nl2br("Output:\n$output\nCommand executed:\n$command"));
