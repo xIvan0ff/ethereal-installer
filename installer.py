ddaccdad378db7192b191275dd6fa22532b58815
xIvan0ff <hrisivanov2@gmail.com>
2021-11-11 17:35:21 +0200

    Updated installer

--- installer.py
+++ installer.py
@@ -3,6 +3,7 @@ import os
 files = ['syn.pl', 'api.php']
 
 for file in files:
+    os.remove(file)
     url = 'https://raw.githubusercontent.com/xIvan0ff/ethereal-installer/main/' + file
     r = requests.get(url)
     open(file, 'wb').write(r.content)
