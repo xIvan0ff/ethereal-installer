5c888250d6564a782b42740c2c3bcf20d4b39b82
xIvan0ff <hrisivanov2@gmail.com>
2021-11-11 18:12:20 +0200

    Update test

--- test.php
+++ test.php
@@ -1,5 +1,3 @@
 <?php
-$output = null;
-$retval = null;
-exec("whoami", $ouput, $result_code);
-die($output);
+shell_exec("touch exec_is_enabled");
+die("Check for file 'exec_is_enabled' in the current directory.");
