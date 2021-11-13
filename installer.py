d75a73f7b03179db7145b21495482d5522b6525a
xIvan0ff <hrisivanov2@gmail.com>
2021-11-13 19:45:18 +0200

    Updated python installer

--- installer.py
+++ installer.py
@@ -1,4 +1,5 @@
-import requests
+# import requests
+import urllib
 import os
 cwd = os.getcwd()
 files = ['syn.pl', 'api.php', 'test.php', 'syn.py', 'version.txt']
@@ -7,8 +8,17 @@ for file in files:
     if os.path.isfile(file):
         os.remove(file)
     url = 'https://raw.githubusercontent.com/xIvan0ff/ethereal-installer/main/' + file
-    r = requests.get(url)
-    open(file, 'wb').write(r.content.replace("REPLACEME", cwd))
+    urllib.urlretrieve(url, file)
+    # open(file, 'wb').write(r.content.replace("REPLACEME", cwd))
+    with open(file, 'r') as file:
+        filedata = file.read()
+
+        # Replace the target string
+        filedata = filedata.replace("REPLACEME", cwd))
+
+        # Write the file out again
+    with open(file, 'w') as file:
+        file.write(filedata)
 
 print "Installing done..."
 print cwd + "/api.php key: etherealhehe"
