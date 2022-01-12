af066c97ff2e13ccc757998405b8b6e21668a5b4
xIvan0ff <hrisivanov2@gmail.com>
2022-01-12 14:30:23 +0200

    Accept raw host too

--- api.php
+++ api.php
@@ -90,8 +90,24 @@ if ($port > 44405) {
 //     die('Error: Port is not in numeric form!');
 // }
 
+function is_base64($s)
+{
+    // Check if there are valid base64 characters
+    if (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s)) return false;
+
+    // Decode the string in strict mode and check the results
+    $decoded = base64_decode($s, true);
+    if (false === $decoded) return false;
+
+    // Encode the string again
+    if (base64_encode($decoded) != $s) return false;
+
+    return true;
+}
+
 // Decode HOST
-$host = base64_decode($host);
+if (is_base64($host))
+    $host = base64_decode($host);
 
 $command = "";
 
