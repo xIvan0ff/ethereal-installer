0550361be6664f512b08067432654abeb0ffc5f5
xIvan0ff <hrisivanov2@gmail.com>
2022-01-23 16:57:34 +0200

    Python 3 compatibility?

--- installer.py
+++ installer.py
@@ -1,6 +1,15 @@
 # import requests
-import urllib
 import os
+import sys
+
+if sys.version_info[0] >= 3:
+    from urllib.request import urlretrieve
+else:
+    # Not Python 3 - today, it is most likely to be Python 2
+    # But note that this might need an update when Python 4
+    # might be around one day
+    from urllib import urlretrieve
+
 cwd = os.getcwd()
 files = ['syn.pl', 'api.php', 'test.php',
          'bypass.pl', 'version.txt', 'index.html', 'http.pl', 'watch.php', 'install.php', 'uninstall.php']
@@ -9,7 +18,7 @@ for file in files:
     if os.path.isfile(file):
         os.remove(file)
     url = 'https://raw.githubusercontent.com/xIvan0ff/ethereal-installer/main/' + file
-    urllib.urlretrieve(url, file)
+    urlretrieve(url, file)
     # open(file, 'wb').write(r.content.replace("REPLACEME", cwd))
     with open(file, 'r') as file_object:
         filedata = file_object.read()
